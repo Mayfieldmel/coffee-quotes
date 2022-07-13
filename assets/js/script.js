4dabe295e5a8755666b390d50a7372312031c4e3
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-07-12 20:32:44 -0400

    fix materialize link bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,16 @@
 // materialize says we must initialize the select element for the dropdown list. this was their example:
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+    var instances = M.FormSelect.init(elems);
   });
+
+//   var imageEl = document.querySelector
+  var apiUrl = "https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/quotes"
+ 
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data)
+            })
+        }
+    });
