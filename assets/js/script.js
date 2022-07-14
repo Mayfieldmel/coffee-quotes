26ee52fb1c975f0a56df6fe248b8ab617c1d7d27
Zach Oda <zjodachow@gmail.com>
2022-07-14 11:37:50 -0400

    quote work started

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,41 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FormSelect.init(elems);
   });
 
-//   var imageEl = document.querySelector
-  var apiUrl = "https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/quotes"
+// //   var imageEl = document.querySelector
+//   var apiUrl = "https://api.quotable.io/random"
  
-    fetch(apiUrl).then(function(response) {
-        if(response.ok) {
-            response.json().then(function(data) {
-                console.log(data)
-            })
-        }
-    });
+//     fetch(apiUrl).then(function(response) {
+//         if(response.ok) {
+//             response.json().then(function(data) {
+//                 console.log(data)
+//             })
+//         }
+//     });
+
+    //   var quoteEl = document.querySelector
+  var apiUrl = "https://api.quotable.io/random"
+ 
+  fetch(apiUrl).then(function(response) {
+      if(response.ok) {
+          response.json().then(function(data) {
+              console.log(data)
+          })
+      }
+  });
+
+  //get "tag" value from dropdown list - 
+  var tagVal = function(event) {
+    var selectEl = document.querySelector("#quote-type")
+    selectEl.addEventListener("change",function(event) {
+      var tagEl = document.getElementById(selectEl).value;
+    })
+  }
+
+  //add "tag" value to end of URL to get quote from that "tag" value
+var tagUrl = function () {
+apiUrlquote.searchParams.append(tagEl);
+var newUrl = apiUrlquote.toString();
+}
+
+
+  //display author & content values in "quote-author" & "quote-content"
