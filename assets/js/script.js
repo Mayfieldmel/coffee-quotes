c3365c39893eb8b9a425f42839e348185bd4fcee
Danielle <danielle@Danielles-MacBook-Air.local>
2022-07-14 19:15:24 -0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
-
-
 // materialize says we must initialize the select element for the dropdown list. this was their example:
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
-  });
 
-//   var imageEl = document.querySelector
-  var apiUrl = "https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/quotes"
- 
-    fetch(apiUrl).then(function(response) {
+
+      var Foodicon = document.querySelector("#icons");
+      var apilink = "https://foodish-api.herokuapp.com/images/pizza/"    
+  
+      fetch(apilink)
+        .then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
+              Foodicon.addEventListener("change",function(event) {
+          
+              });
                 console.log(data)
             })
         }
+    });
+  
+  
+
     });
