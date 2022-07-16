015a6199205ec8596c6b756965a327396ceba4bc
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-07-16 10:09:13 -0400

    add zach's code

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,41 +65,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
     formEl.addEventListener("submit", getFoodImage);
 
-//     //   var quoteEl = document.querySelector
-//   var apiUrl = "https://api.quotable.io/random"
+    //   var quoteEl = document.querySelector
+  var apiUrl = "https://api.quotable.io/random"
  
-//   fetch(apiUrl).then(function(response) {
-//       if(response.ok) {
-//           response.json().then(function(data) {
-//               console.log(data)
-//           })
-//       }
-//   });
+  fetch(apiUrl).then(function(response) {
+      if(response.ok) {
+          response.json().then(function(data) {
+              console.log(data)
+          })
+      }
+  });
 
-//   //get "tag" value from dropdown list - 
-//   var tagVal = function() {
-//     var selectEl = document.querySelector("#quote-type")
-//     selectEl.addEventListener("change",function() {
-//     var tagEl = document.getElementById(selectEl).value;
-//     return tagEl;
-//     });
-//     console.log(tagEl)
-//   }
+  //get "tag" value from dropdown list - 
+  var tagVal = function() {
+    var selectEl = document.querySelector("#quote-type")
+    selectEl.addEventListener("change",function() {
+    var tagEl = document.getElementById(selectEl).value;
+    return tagEl;
+    });
+    console.log(tagEl)
+  }
 
-//   //add "tag" value to end of URL to get quote from that "tag" value
-// var tagUrl = function () {
-// apiUrlquote.searchParams.append(tagEl);
-// var newUrl = apiUrlquote.toString();
-// }
+  //add "tag" value to end of URL to get quote from that "tag" value
+var tagUrl = function () {
+apiUrlquote.searchParams.append(tagEl);
+var newUrl = apiUrlquote.toString();
+}
 
 
-//   //display author & content values in "quote-author" & "quote-content"
-//   var addAuthor = function() {
-//     var authorSpace = document.querySelector("#quote-author");
-//     var selectedAuthor = document.author.
-//     selectedAuthor.textContent = document.author.value;
+  //display author & content values in "quote-author" & "quote-content"
+  var addAuthor = function() {
+    var authorSpace = document.querySelector("#quote-author");
+    var selectedAuthor = document.author.
+    selectedAuthor.textContent = document.author.value;
     
 
 
-//   }
+  }
   
