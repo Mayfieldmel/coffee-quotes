b8cf16eb3be2ade7bf17de5815d3bcb65b369bda
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-07-18 17:41:28 -0400

    fetch quote based on slected topic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
     var formEl = document.querySelector("#form-box");
     var submitBtnEl = document.querySelector("#submit");
     var foodImageEl = document.querySelector("#food-image");
+    var quoteTypeEl = document.querySelector("#quote-type");
     var selectFoodEl = document.querySelector("#select-food");
     var authorSpace = document.querySelector("#quote-author");
     var quoteSpace = document.querySelector("#quote");
     var foodTypeInput 
+    var quoteTypeInput
 
 // materialize says we must initialize the select element for the dropdown list. 
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,7 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
    // capture user food choice
     var getFoodType = function() {
        foodTypeInput = event.target.value.trim();
-        console.log(event.target.value);
         };
 
     // display random food image
@@ -46,38 +47,46 @@ document.addEventListener('DOMContentLoaded', function() {
             })    
     };
 
-var getQuote = function() {
-var quoteApiUrl = "https://api.quotable.io/random";
-  fetch(quoteApiUrl).then(function(response) {
-      if(response.ok) {
-          response.json().then(function(data) {
-              console.log(data)
-              displayQuote(data);
-              addAuthor(data)
-          })
-      }
-  })
+//capture quote topic
+var getQuoteType = function()  {
+    quoteTypeInput = event.target.value.trim();
+        console.log(quoteTypeInput);
 };
 
-  //display fetched quote and quote author 
+//display fetched quote and quote author 
 var displayQuote = function(data) {
-        var randomQuote = data.content;
-        console.log(randomQuote); 
-        quoteSpace.textContent =  '"' + randomQuote + '"';
-  }
-  var addAuthor = function(data) {
+    var randomQuote = data.content;
+    console.log(randomQuote); 
+    quoteSpace.textContent =  '"' + randomQuote + '"';
+  };
+var displayAuthor = function(data) {
     var quoteAuthor = data.author;
     console.log(quoteAuthor);
     authorSpace.textContent = quoteAuthor;
   };
 
+//   fetch quote from API
+var getQuote = function() {
+    console.log(quoteTypeInput);
+    var quoteApiUrl = "https://api.quotable.io/random?tags=" + quoteTypeInput;
+      fetch(quoteApiUrl).then(function(response) {
+          if(response.ok) {
+              response.json().then(function(data) {
+                  displayQuote(data);
+                  displayAuthor(data)
+              })
+          }
+      })
+    };
+    
 
 var getData = function(event) {
     event.preventDefault();
     getFoodImage(getFoodType);
-    getQuote();
+    getQuote(getQuoteType);
 }
 
 // event listeners
+quoteTypeEl.addEventListener("change", getQuoteType)
 selectFoodEl.addEventListener("change", getFoodType);
 formEl.addEventListener("submit", getData);
