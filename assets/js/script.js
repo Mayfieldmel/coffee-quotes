07b25c4997565f3fc6947ce25d0b8276a8e4c79d
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-07-18 19:11:10 -0400

    saved food and quote objects to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 // variables defined
-    var formEl = document.querySelector("#form-box");
-    var submitBtnEl = document.querySelector("#submit");
-    var foodImageEl = document.querySelector("#food-image");
-    var quoteTypeEl = document.querySelector("#quote-type");
-    var selectFoodEl = document.querySelector("#select-food");
-    var authorSpace = document.querySelector("#quote-author");
-    var quoteSpace = document.querySelector("#quote");
-    var foodTypeInput 
-    var quoteTypeInput
-
-<<<<<<< HEAD
+var formEl = document.querySelector("#form-box");
+var submitBtnEl = document.querySelector("#submit");
+var foodImageEl = document.querySelector("#food-image");
+var quoteTypeEl = document.querySelector("#quote-type");
+var selectFoodEl = document.querySelector("#select-food");
+var authorSpace = document.querySelector("#quote-author");
+var quoteSpace = document.querySelector("#quote");
+var priorSearchesEl = document.querySelector("#prior-searches");
+var foodTypeInput 
+var quoteTypeInput
+var foodArr = [];
+var foodObj = {}
+var quoteArr = [];
+var quoteObj = {}
+    
 // materialize says we must initialize the select element for the dropdown list. 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
@@ -24,7 +28,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // display random food image
     var displayImage = function(data) {
         var randomImage = data.image;
-        console.log(randomImage);
         foodImageEl.setAttribute("src", randomImage)
     }
     
@@ -35,8 +38,8 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch(foodApiUrl).then(function(response) {
             if(response.ok) {
                 response.json().then(function(data) {
-                    console.log(data);
                     displayImage(data);
+                    saveFood(foodTypeInput, foodApiUrl);
                 })
             } else {
                 alert('Error: ' + response.statusText);
@@ -74,7 +77,8 @@ var getQuote = function() {
           if(response.ok) {
               response.json().then(function(data) {
                   displayQuote(data);
-                  displayAuthor(data)
+                  displayAuthor(data);
+                  saveQuote(quoteTypeInput, quoteApiUrl);
               })
           }
       })
@@ -87,137 +91,67 @@ var getData = function(event) {
     getQuote(getQuoteType);
 }
 
+
+// save search links
+
+    // create link
+    // var priorMeal = document.createElement("li");
+    //     priorSearchesEl.appendChild(priorMeal);
+    // var priorMealLink = document.createElement("a");
+    //     priorMealLink.textContent = getFoodType + getQuoteType;
+    //     priorMealLink.classList = "links";
+    //     priorMealLink.setAttribute("href", "#")
+    //     priorMeal.appendChild(priorMealLink);
+   
+// store food data in local storage
+function saveFood(foodType, foodUrl) {
+    foodObj = {
+        type: foodType,
+        url: foodUrl
+    };
+    localStorage.getItem("foodArr");
+    if(!foodArr) {
+        localStorage.setItem("foodArr", foodObj);
+    } else {
+        foodArr.push(foodObj);
+        localStorage.setItem("foodArr", JSON.stringify(foodArr));
+    }
+};
+// store quote data in local storage
+function saveQuote(quoteTopic, quoteUrl) {
+    quoteObj = {
+        type: quoteTopic,
+        url: quoteUrl
+    };
+    localStorage.getItem("quoteArr");
+    if(!quoteArr) {
+        localStorage.setItem("quoteArr", quoteObj);
+    } else {
+        quoteArr.push(quoteObj);
+        localStorage.setItem("quoteArr", JSON.stringify(quoteArr));
+    }
+};
+
+// weather dashboard code:  // display data from local storage
+// var displaySavedSearches = function() {
+//     var cityHistoryStr = localStorage.getItem("city");
+//     if(cityHistoryStr) {
+//         cityHistoryArr = cityHistoryStr.split(",");
+//         console.log(cityHistoryArr);
+//         // display btn for each saved search
+//         for (let i = 0; i < cityHistoryArr.length; i++) {
+//             var searchHistoryBtn = document.createElement("button");
+//             searchHistoryBtn.textContent = cityHistoryArr[i];
+//             searchHistoryBtn.classList = "btn";
+//             searchHistoryEl.appendChild(searchHistoryBtn);
+//         }
+//     } else {};
+// }
+
 // event listeners
 quoteTypeEl.addEventListener("change", getQuoteType)
 selectFoodEl.addEventListener("change", getFoodType);
 formEl.addEventListener("submit", getData);
-=======
-
-// materialize says we must initialize the select element for the dropdown list. this was their example:
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems);
-});
-
-// //   var imageEl = document.querySelector
-//   var apiUrl = "https://api.quotable.io/random"
-
-//     fetch(apiUrl).then(function(response) {
-//         if(response.ok) {
-//             response.json().then(function(data) {
-//                 console.log(data)
-//             })
-//         }
-//     });
-// 
-var formEl = document.querySelector("#form-box");
-var submitBtnEl = document.querySelector("#submit");
-var foodImageEl = document.querySelector("#food-image");
-
-var selectFoodEl = document.querySelector("#select-food");
-// var foodTypeInput = selectFoodEl.value;
-
-// get food type
-var getFoodType = function () {
-  console.log(selectFoodEl)
-  console.log(event.target);
-};
-
-selectFoodEl.addEventListener("change", getFoodType)
-
-var displayImage = function (data) {
-  var randomImage = data.image;
-  console.log(randomImage);
-  foodImageEl.setAttribute("src", randomImage)
-}
-
-var getFoodImage = function (event) {
-  console.log("click");
-  event.preventDefault();
-  // console.log(foodTypeInput)
-  // var apiLink = "https://foodish-api.herokuapp.com/images/api" + foodTypeInput + "/";
-  var apiLink = "https://foodish-api.herokuapp.com/api/images/burger/"
-  fetch(apiLink).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-        displayImage(data);
-      })
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  })
-    .catch(function (error) {
-      alert('Unable to connect to foodish database');
-      console.log(error);
-    })
-
-
-};
-
-var userVal = function () {
-  var userEl = document.getElementById(selectFoodEl).value;
-  return userEl;
-};
-console.log(userEl)
-userVal();
-
-var tagLink = function () {
-  apiLinkimage.searchParams.append(userEl);
-}
-
-selectFoodEl.addEventListener("submit", getFoodImage)
-
-//   var quoteEl = document.querySelector
-var apiUrl = "https://api.quotable.io/random"
-
-fetch(apiUrl).then(function (response) {
-  if (response.ok) {
-    response.json().then(function (data) {
-      console.log(data)
-    })
-  }
-});
-
-//get "tag" value from dropdown list - 
-var tagVal = function () {
-  var selectEl = document.querySelector("#quote-type")
-  selectEl.addEventListener("change", function () {
-    var tagEl = document.getElementById(selectEl).value;
-    return tagEl;
-  });
-  console.log(tagEl)
-}
-
-//add "tag" value to end of URL to get quote from that "tag" value
-var tagUrl = function () {
-  apiUrlquote.searchParams.append(tagEl);
-  var newUrl = apiUrlquote.toString();
-}
-
-
-//display author & content values in "quote-author" & "quote-content"
-var addAuthor = function () {
-  var authorSpace = document.querySelector("#quote-author");
-  var selectedAuthor = document.author.
-    selectedAuthor.textContent = document.author.value;
 
 
 
-}
-
-// local storage for previous searches 
-//const selectedFood = document.getElementById('select-food',);
-
-//selectedFood.addEventListener('change', (event) => {
-// let currentArea = event.target.value;
-//localStorage.setItem('Food', currentArea);
-//});
-
-var foodChoice = document.getElementById("select")
-var quoteChoice = document.getElementById(".Quote")
-var Choices = [quoteChoice, foodChoice]
-//console.log(Choices);
-console.log(foodChoice)
-console.log(quoteChoice)
->>>>>>> e7b152080fe16a13f52aa1395d28008cf2ea1600
