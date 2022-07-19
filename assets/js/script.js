6f514ff29923761a7aa726e912d35b9d59bee3f8
Zach Oda <zjodachow@gmail.com>
2022-07-19 17:51:17 -0400

    updated Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,135 +1,102 @@
 // variables defined
-    var formEl = document.querySelector("#form-box");
-    var submitBtnEl = document.querySelector("#submit");
-    var foodImageEl = document.querySelector("#food-image");
-    var selectFoodEl = document.querySelector("#select-food");
-    var authorSpace = document.querySelector("#quote-author");
-    var quoteSpace = document.querySelector("#quote");
-    var errorModal = document.getElementById("#error-modal");
-    // var foodTypeInput = selectFoodEl.value;
+var formEl = document.querySelector("#form-box");
+var submitBtnEl = document.querySelector("#submit");
+var foodImageEl = document.querySelector("#food-image");
+var quoteTypeEl = document.querySelector("#quote-type");
+var selectFoodEl = document.querySelector("#select-food");
+var authorSpace = document.querySelector("#quote-author");
+var quoteSpace = document.querySelector("#quote");
+var foodTypeInput 
+var quoteTypeInput
 
 // materialize says we must initialize the select element for the dropdown list. 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
-  });
+var elems = document.querySelectorAll('select');
+var instances = M.FormSelect.init(elems);
+});
 
-   // capture user food choice
-   var userVal= function() {
-    console.log("click"); }
-
-
-    // get food type
-    var getFoodType = function() {
-        console.log(selectFoodEl)
-        console.log(event.target);
-        };
-  
-        selectFoodEl.addEventListener("change", getFoodType)
-   
-
-    // display random food image
-    var displayImage = function(data) {
-        var randomImage = data.image;
-        console.log(randomImage);
-        foodImageEl.setAttribute("src", randomImage)
-    }
-    
-    // fetch food image from API
-    var getFoodImage = function(event) {
-        // console.log(foodTypeInput)
-        // var apiLink = "https://foodish-api.herokuapp.com/images/api" + foodTypeInput + "/";
-        var apiLink = "https://foodish-api.herokuapp.com/api/images/burger/"
-        fetch(apiLink).then(function(response) {
-            if(response.ok) {
-                response.json().then(function(data) {
-                    console.log(data);
-                    displayImage(data);
-                })
-            } else {
-                alert('Error: ' + response.statusText);
-              }
-        })
-            .catch(function (error) {
-              alert('Unable to connect to foodish database');
-              console.log(error);
-            })
-     
-           
+// capture user food choice
+var getFoodType = function() {
+   foodTypeInput = event.target.value.trim();
     };
-    
-   
 
-
-  //get "tag" value from dropdown list - 
-//   var tagVal = function() {
-//     var selectEl = document.querySelector("#quote-type")
-//     selectEl.addEventListener("change", function() {
-//     var tagEl = selectEl.textContent;
-//     return tagEl;
-//     });
-//     console.log(tagEl)
-//   }; tagVal();
-
-  //add "tag" value to end of URL to get quote from that "tag" value
-// var tagUrl = function () {
-// apiUrlquote.searchParams.append(tagEl);
-// var newUrl = apiUrlquote.toString();
-// // }
-          var userVal= function(){
-                var userEl = document.getElementById(selectFoodEl).value;
-                return userEl;
-              };
-          console.log(userEl)
-             userVal();
-
-            var tagLink =function () {
-              apiLinkimage.searchParams.append(userEl);
-            }
-            
-    selectFoodEl.addEventListener("submit", getFoodImage)
-
-// fetch quote from API
-var getQuote = function() {
-var apiUrl = "https://api.quotable.io/random"
- 
-  fetch(apiUrl).then(function(response) {
-      if(response.ok) {
-          response.json().then(function(data) {
-              console.log(data)
-              displayQuote(data);
-              addAuthor(data)
-          })
-      }
-  });
-};
-
-  //display fetched quote and quote author 
-var displayQuote = function(data) {
-        var randomQuote = data.content;
-        console.log(randomQuote); 
-        quoteSpace.textContent =  '"' + randomQuote + '"';
-  }
-  var addAuthor = function(data) {
-    var quoteAuthor = data.author
-    console.log(quoteAuthor)
-    authorSpace.textContent = quoteAuthor;
-  };
-
-
-var getData = function() {
-    event.preventDefault();
-    getFoodImage();
-    getQuote();
+// display random food image
+var displayImage = function(data) {
+    var randomImage = data.image;
+    console.log(randomImage);
+    foodImageEl.setAttribute("src", randomImage)
 }
 
- // create modal that alerts user to select food type
- document.querySelector("#show-error-modal").addEventListener("click", function() {
-  if (getFoodType === "" || getQuote === "") {
-    console.log("error")
+// fetch food image from API
+var getFoodImage = function() {
+    console.log(foodTypeInput);
+    var foodApiUrl = "https://foodish-api.herokuapp.com/api/images/" + foodTypeInput + "/";
+    fetch(foodApiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                displayImage(data);
+            })
+        } else {
+            alert('Error: ' + response.statusText);
+          }
+    })
+        .catch(function (error) {
+          alert('Unable to connect to foodish database');
+          console.log(error);
+        })    
+};
+
+//capture quote topic
+var getQuoteType = function()  {
+quoteTypeInput = event.target.value.trim();
+    console.log(quoteTypeInput);
+};
+
+//display fetched quote and quote author 
+var displayQuote = function(data) {
+var randomQuote = data.content;
+console.log(randomQuote); 
+quoteSpace.textContent =  '"' + randomQuote + '"';
+};
+var displayAuthor = function(data) {
+var quoteAuthor = data.author;
+console.log(quoteAuthor);
+authorSpace.textContent = quoteAuthor;
+};
+
+//   fetch quote from API
+var getQuote = function() {
+console.log(quoteTypeInput);
+var quoteApiUrl = "https://api.quotable.io/random?tags=" + quoteTypeInput;
+  fetch(quoteApiUrl).then(function(response) {
+      if(response.ok) {
+          response.json().then(function(data) {
+              displayQuote(data);
+              displayAuthor(data)
+          })
+      }
+  })
+};
+
+// create modal that alerts user to select food type
+document.querySelector("#submit").addEventListener("click", function() {
+  if (foodTypeInput === "" || quoteTypeInput === "") {
+    console.log("error");
+    //document.getElementbyId("#error-modal").classList.remove("none");
   }
 });
 
-  
 
+var getData = function(event) {
+event.preventDefault();
+getFoodImage(getFoodType);
+getQuote(getQuoteType);
+}
+
+// event listeners
+quoteTypeEl.addEventListener("change", getQuoteType)
+selectFoodEl.addEventListener("change", getFoodType);
 formEl.addEventListener("submit", getData);
+
+ 
